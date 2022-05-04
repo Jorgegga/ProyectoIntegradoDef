e53d9b70b9f2c96b46ac3cabbf3aa6a49198c50a
Jorgegga <65369692+Jorgegga@users.noreply.github.com>
2022-05-04 14:47:00 +0200

    empezar autores

---
+++ app/src/main/java/com/example/proyectointegradodef/musica/autor/AutorFragment.kt
@@ -0,0 +1,40 @@
+package com.example.proyectointegradodef.musica.autor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.proyectointegradodef.R
+import com.example.proyectointegradodef.databinding.FragmentAutorBinding
+
+class AutorFragment : Fragment() {
+    lateinit var binding: FragmentAutorBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentAutorBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() : AutorFragment{
+
+            return AutorFragment()
+        }
+    }
+}
