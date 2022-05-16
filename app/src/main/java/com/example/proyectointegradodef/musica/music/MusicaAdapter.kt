c64ded1a43ab1b92bea0e06a5e59b3f93d212ce2
Jorgegga <65369692+Jorgegga@users.noreply.github.com>
2022-05-16 14:52:09 +0200

    rehaciendo recycler onclick

--- app/src/main/java/com/example/proyectointegradodef/musica/music/MusicaAdapter.kt
+++ app/src/main/java/com/example/proyectointegradodef/musica/music/MusicaAdapter.kt
@@ -1,17 +1,35 @@
 package com.example.proyectointegradodef.musica.music
 
+import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.example.proyectointegradodef.R
-import com.example.proyectointegradodef.models.ReadMusica
+import com.example.proyectointegradodef.databinding.MusicaLayoutBinding
+import com.example.proyectointegradodef.glide.GlideApp
 import com.example.proyectointegradodef.models.ReadMusicaAlbumAutor
+import com.google.firebase.storage.FirebaseStorage
 
-class MusicaAdapter(private val lista: ArrayList<ReadMusicaAlbumAutor>): RecyclerView.Adapter<MusicaViewHolder>(){
+class MusicaAdapter(private val lista: ArrayList<ReadMusicaAlbumAutor>): RecyclerView.Adapter<MusicaAdapter.MusicaViewHolder>(){
+
+    private lateinit var mListener: onItemClickListener
+
+    interface onItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    fun setOnItemClickListener(listener: onItemClickListener){
+        mListener = listener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicaViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val v = inflater.inflate(R.layout.musica_layout, parent, false)
-        return MusicaViewHolder(v)
+        return MusicaViewHolder(v, mListener)
     }
 
     override fun onBindViewHolder(holder: MusicaViewHolder, position: Int) {
@@ -21,6 +39,39 @@ class MusicaAdapter(private val lista: ArrayList<ReadMusicaAlbumAutor>): Recycle
 
     override fun getItemCount(): Int {
         return lista.size
+    }
+
+    class MusicaViewHolder(v: View, listener: onItemClickListener): RecyclerView.ViewHolder(v){
+        private val binding = MusicaLayoutBinding.bind(v)
+        var storageFire = FirebaseStorage.getInstance()
+
+        init{
+            itemView.setOnClickListener {
+                listener.onItemClick(adapterPosition)
+            }
+        }
+
+        fun render(musica : ReadMusicaAlbumAutor){
+            binding.tvTitulo.text = musica.nombre
+            binding.tvAlbum.text = musica.album
+            binding.tvAutor.text = musica.autor
+            val gsReference2 = storageFire.getReferenceFromUrl(musica.portada + ".png")
+            val option = RequestOptions().error(R.drawable.default_album)
+            GlideApp.with(itemView.context).load(gsReference2).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).apply(option).into(binding.ivMusica)
+
+
+
+        }
+
+        /*AppUse.id = musica.id
+        AppUse.autor_id = musica.autor_id
+        AppUse.nombre = musica.nombre
+        AppUse.autor = musica.autor
+        AppUse.cancion = musica.ruta
+        AppUse.reproduciendo.value = true
+        AppUse.reproduciendo.value = false*/
+
+
     }
 
 }
--- app/src/main/java/com/example/proyectointegradodef/musica/music/MusicaViewHolder.kt
+++ app/src/main/java/com/example/proyectointegradodef/musica/music/MusicaViewHolder.kt
@@ -7,14 +7,13 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.proyectointegradodef.R
 import com.example.proyectointegradodef.databinding.MusicaLayoutBinding
 import com.example.proyectointegradodef.glide.GlideApp
-import com.example.proyectointegradodef.models.ReadMusica
 import com.example.proyectointegradodef.models.ReadMusicaAlbumAutor
-import com.example.proyectointegradodef.preferences.AppUse
 import com.google.firebase.storage.FirebaseStorage
 
 class MusicaViewHolder(v: View ): RecyclerView.ViewHolder(v){
     private val binding = MusicaLayoutBinding.bind(v)
     var storageFire = FirebaseStorage.getInstance()
+
 
     fun render(musica : ReadMusicaAlbumAutor){
         binding.tvTitulo.text = musica.nombre
@@ -23,16 +22,18 @@ class MusicaViewHolder(v: View ): RecyclerView.ViewHolder(v){
         val gsReference2 = storageFire.getReferenceFromUrl(musica.portada + ".png")
         val option = RequestOptions().error(R.drawable.default_album)
         GlideApp.with(itemView.context).load(gsReference2).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).apply(option).into(binding.ivMusica)
-
-        binding.musicCardView.setOnClickListener {
-            AppUse.id = musica.id
-            AppUse.autor_id = musica.autor_id
-            AppUse.nombre = musica.nombre
-            AppUse.autor = musica.autor
-            AppUse.cancion = musica.ruta
-            AppUse.reproduciendo.value = true
-            AppUse.reproduciendo.value = false
-        }
     }
+
+
+
+
+        /*AppUse.id = musica.id
+        AppUse.autor_id = musica.autor_id
+        AppUse.nombre = musica.nombre
+        AppUse.autor = musica.autor
+        AppUse.cancion = musica.ruta
+        AppUse.reproduciendo.value = true
+        AppUse.reproduciendo.value = false*/
+
 
 }
