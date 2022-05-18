98c467b7c23fe3809f686db21a2517d9676e59f9
Jorgegga <65369692+Jorgegga@users.noreply.github.com>
2022-05-18 14:52:12 +0200

    filtrar musica album

--- app/src/main/java/com/example/proyectointegradodef/musica/album/AlbumActivity.kt
+++ app/src/main/java/com/example/proyectointegradodef/musica/album/AlbumActivity.kt
@@ -2,22 +2,30 @@ package com.example.proyectointegradodef.musica.album
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.example.proyectointegradodef.R
 import com.example.proyectointegradodef.databinding.ActivityAlbumBinding
 import com.example.proyectointegradodef.glide.GlideApp
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.example.proyectointegradodef.models.ReadAlbum
+import com.example.proyectointegradodef.models.ReadMusica
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 
 class AlbumActivity : AppCompatActivity() {
     lateinit var binding: ActivityAlbumBinding
     lateinit var db: FirebaseDatabase
-    lateinit var reference: DatabaseReference
-    lateinit var reference2: DatabaseReference
+    lateinit var referenceAlbum: DatabaseReference
+    lateinit var referenceAutor: DatabaseReference
+    lateinit var referenceMusic: DatabaseReference
     var storageFire = FirebaseStorage.getInstance()
+
+    var music: MutableList<ReadMusica> = ArrayList()
+
+    var album_id = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +37,13 @@ class AlbumActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         initDb()
         introducirDatos()
+        recogerDatosMusica()
+
     }
 
     fun introducirDatos(){
         val bundle = intent.extras
-
+        album_id = bundle!!.getInt("id", 0)
         val gsReference2 = storageFire.getReferenceFromUrl(bundle!!.getString("portada", "Default") + ".png")
         val option = RequestOptions().error(R.drawable.default_album)
         GlideApp.with(this).load(gsReference2).diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true).apply(option).into(binding.ivAlbum)
@@ -41,10 +51,42 @@ class AlbumActivity : AppCompatActivity() {
 
     }
 
+    fun recogerDatosMusica(){
+        music.clear()
+        referenceMusic.get()
+        referenceMusic.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                music.clear()
+                for(messageSnapshot in snapshot.children){
+                    val tema = messageSnapshot.getValue<ReadMusica>(ReadMusica::class.java)
+                    if(tema != null){
+                        music.add(tema)
+                    }
+                }
+                rellenarDatos()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+    }
+
+    fun rellenarDatos(){
+        var musicaAgrupada = music.groupBy { it.album_id }
+        music = musicaAgrupada[album_id] as ArrayList
+        for(x in music){
+            Log.d("aaaaaaaaaaaaaaaaaaaaa", x.nombre)
+        }
+    }
+
     private fun initDb(){
         db = FirebaseDatabase.getInstance("https://proyectointegradodam-eef79-default-rtdb.europe-west1.firebasedatabase.app/")
-        reference = db.getReference("albums")
-        reference2 = db.getReference("autors")
+        referenceAlbum = db.getReference("albums")
+        referenceAutor = db.getReference("autors")
+        referenceMusic = db.getReference("music")
     }
 
     override fun onSupportNavigateUp() : Boolean{
